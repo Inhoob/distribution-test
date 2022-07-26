e9dc60436d5536284cd6741a06d5cf2241937588
Inhoob <inhoob@gmail.com>
2022-07-26 13:50:08 +0900

    distribution 실험

--- src/components/Discussions.js
+++ src/components/Discussions.js
@@ -1,11 +1,17 @@
 import { Discussion } from "./Discussion";
-
+import React from "react";
 export const Discussions = ({ discussions, deleteDiscussion }) => {
   return (
     <section className="discussion__wrapper">
       <ul className="discussions__container">
         {discussions.map((discussion) => {
-          return <Discussion key={discussion.id} discussion={discussion} deleteDiscussion={deleteDiscussion}/>
+          return (
+            <Discussion
+              key={discussion.id}
+              discussion={discussion}
+              deleteDiscussion={deleteDiscussion}
+            />
+          );
         })}
       </ul>
     </section>
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./docs"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  devServer: {
+    host: "localhost",
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+  ],
+};
